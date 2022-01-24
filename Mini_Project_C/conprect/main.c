e715081f4b180bbccbc8b2e2087fc611a3e0fdf5
Shaik-Amaan <72979637+Shaik-Amaan@users.noreply.github.com>
2022-01-24 18:45:37 +0530

    Add files via upload

---
+++ Mini_Project_C/conprect/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int PaymentEMI();           /* not added parameters yet*/
+int StructureCost();
+int Time();
+void ScanDetails();
+int TotalCost();
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
